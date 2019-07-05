d2197a929c8fcb05a74fce07a0e2b1382fb86aad
En Yang Pang <epang1@my.westga.edu>
2019-07-05 04:19:27 -0400

    Tweaked the routing tests to model clicking

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -1,8 +1,5 @@
 import { browser, element, by } from 'protractor';
 
-
-
-
 describe('Routing Tests', () => {
   it('should navigate to welcome page', () => {
     browser.get('/');
@@ -11,11 +8,13 @@ describe('Routing Tests', () => {
   
   it('should navigate to registration page', () => {
     browser.get('/');
-    expect(element(by.id('registration')).getText()).toBe('Registration');
+    element(by.id('registration')).click();
+    expect(element(by.id('register')).getText()).toBe('Register Players');
   });
   
   it('should navigate to brackets page', () => {
     browser.get('/');
-    expect(element(by.id('brackets')).getText()).toBe('Brackets');
+    element(by.id('brackets')).click();
+    expect(element(by.id('subpageTitle')).getText()).toBe('Brackets');
   });
 });
