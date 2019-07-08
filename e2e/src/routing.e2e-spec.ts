3c33674c46114c51af22434b5d875000089d0ab9
En Yang Pang <epang1@my.westga.edu>
2019-07-08 08:47:17 -0400

    Added tests for errors in website usage

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -1,19 +1,20 @@
 import { browser, element, by } from 'protractor';
 
 describe('Routing Tests', () => {
-  it('should navigate to welcome page', () => {
+  beforeEach(() => {
     browser.get('/');
+  });
+  
+  it('should navigate to welcome page', () => {
     expect(element(by.tagName('h2')).getText()).toBe('Brackets App');
   });
   
   it('should navigate to registration page', () => {
-    browser.get('/');
     element(by.id('registration')).click();
     expect(element(by.id('register')).getText()).toBe('Register Players');
   });
   
   it('should navigate to brackets page', () => {
-    browser.get('/');
     element(by.id('brackets')).click();
     expect(element(by.id('subpageTitle')).getText()).toBe('Brackets');
   });
