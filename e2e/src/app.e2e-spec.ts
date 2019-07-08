9972b15c254fe6e35abb18c3a05c18754de44463
En Yang Pang <epang1@my.westga.edu>
2019-07-08 07:21:48 -0400

    Added a bunch of success tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -86,5 +86,98 @@ describe('Success scenarios', () => {
     expect(element(by.id('message')).getText()).toBe('Leia,Luke,Lando,Han,Chewy,R2D2,C3P0,Vader');
   });
   
+  it('should successfully match two players up in a bracket', () => {
+    element(by.id('registration')).click();
+    element(by.id('contestant0')).sendKeys('Shen');
+    element(by.id('contestant1')).sendKeys('Zed');
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    expect(element(by.id('match1')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Shen');
+    expect(element(by.id('match1')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Zed');
+  });
   
+  it('should successfully match four players up in a bracket', () => {
+    element(by.id('registration')).click();
+    element(by.id('contestant0')).sendKeys('Benjamin');
+    element(by.id('contestant1')).sendKeys('Ratul');
+    element(by.id('contestant2')).sendKeys('Scott');
+    element(by.id('contestant3')).sendKeys('Daniel');
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    expect(element(by.id('match1')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Benjamin');
+    expect(element(by.id('match1')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Ratul');
+    expect(element(by.id('match2')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Scott');
+    expect(element(by.id('match2')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Daniel');
+  });
+  
+  it('should successfully match eight players up in a bracket', () => {
+    element(by.id('registration')).click();
+    element(by.id('contestant0')).sendKeys('Naruto');
+    element(by.id('contestant1')).sendKeys('Sasuke');
+    element(by.id('contestant2')).sendKeys('Sakura');
+    element(by.id('contestant3')).sendKeys('Kakashi');
+    element(by.id('contestant4')).sendKeys('Sai');
+    element(by.id('contestant5')).sendKeys('Yamato');
+    element(by.id('contestant6')).sendKeys('Hinata');
+    element(by.id('contestant7')).sendKeys('Neji');
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    expect(element(by.id('match1')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Naruto');
+    expect(element(by.id('match1')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Sasuke');
+    expect(element(by.id('match2')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Sakura');
+    expect(element(by.id('match2')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Kakashi');
+    expect(element(by.id('match3')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Sai');
+    expect(element(by.id('match3')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Yamato');
+    expect(element(by.id('match4')).all(by.tagName('input')).get(0).getAttribute('value')).toBe('Hinata');
+    expect(element(by.id('match4')).all(by.tagName('input')).get(1).getAttribute('value')).toBe('Neji');
+  });
+  
+  it('should successfully declare a champion in a two-player tournament', () => {
+    element(by.id('registration')).click();
+    element(by.id('contestant0')).sendKeys('Shen');
+    element(by.id('contestant1')).sendKeys('Zed');
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(0).click();
+    element(by.id('completeRound')).click();
+    expect(element(by.id('champion')).getText()).toBe('Winner: Shen');
+  });
+  
+  it('should successfully declare a champion in a four-player tournament', () => {
+    element(by.id('registration')).click();
+    element(by.id('fill4')).click();
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(0).click();
+    element(by.id('match2')).all(by.tagName('input')).get(1).click();
+    element(by.id('completeRound')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(1).click();
+    element(by.id('completeRound')).click();
+    expect(element(by.id('champion')).getText()).toBe('Winner: Ringo');
+  });
+  
+  it('should successfully declare a champion in an eight-player tournament', () => {
+    element(by.id('registration')).click();
+    element(by.id('contestant0')).sendKeys('Irelia');
+    element(by.id('contestant1')).sendKeys('Trundle');
+    element(by.id('contestant2')).sendKeys('Kennen');
+    element(by.id('contestant3')).sendKeys('Vladimir');
+    element(by.id('contestant4')).sendKeys('Sion');
+    element(by.id('contestant5')).sendKeys('Maokai');
+    element(by.id('contestant6')).sendKeys('Chogath');
+    element(by.id('contestant7')).sendKeys('Nasus');
+    element(by.id('submit')).click();
+    element(by.id('brackets')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(0).click();
+    element(by.id('match2')).all(by.tagName('input')).get(1).click();
+    element(by.id('match3')).all(by.tagName('input')).get(1).click();
+    element(by.id('match4')).all(by.tagName('input')).get(1).click();
+    element(by.id('completeRound')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(0).click();
+    element(by.id('match2')).all(by.tagName('input')).get(1).click();
+    element(by.id('completeRound')).click();
+    element(by.id('match1')).all(by.tagName('input')).get(1).click();
+    element(by.id('completeRound')).click();
+    expect(element(by.id('champion')).getText()).toBe('Winner: Nasus');
+  });
 });
